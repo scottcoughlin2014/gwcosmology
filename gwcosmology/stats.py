e7350b2e669a7ba6148797410457917aadcb38ab
Maya Fishbach <maya.fishbach@ligo.org>
2019-02-08 14:46:48 -0600

    new code to calculate summary statistics

--- gwcosmology/stats.py
+++ gwcosmology/stats.py
@@ -1,42 +1,43 @@
 from numpy import *
-from scipy.interpolate import splev, splrep, UnivariateSpline, interp1d
-from scipy.integrate import cumtrapz
-from scipy.optimize import fmin
 
-def HDI(credint, y, x):
-    """Calculate the highest posterior probability interval
+def HDI(credint, pdfs, xs):
+    """Calculate the highest posterior probability interval by a waterfilling algorithm
 
     Parameters:
 
         credint (float):
             the desired interval, must be less than 1
 
-        y (numpy array):
+        pdfs (numpy array):
             the PDF values evaluated at grid points x
 
-        x (numpy array, same dimensions as y):
+        xs (numpy array, same dimensions as y):
             the grid points at which the PDF values y are evaluated
 
     Returns:
 
         the two endpoints of the smallest interval which encloses credint of the posterior probability
     """
-    cdfvals = cumtrapz(y,x)
-    sel = cdfvals > 0.
-    x = x[1:][sel]
-    cdfvals = cdfvals[sel]
-    cdfvals /= cdfvals[-1] #in case the posterior is not normalized
-    ppf = interp1d(cdfvals,x,fill_value = 0.,bounds_error=False)
-    def intervalWidth(lowTailPr):
-        ret = ppf(credint + lowTailPr) - ppf(lowTailPr)
-        if (ret > 0.):
-            return ret
+    order = argsort(pdfs)[::-1] #largest to smallest
+    last_index = len(pdfs)-1
+    mass = 0.0
+    for n in order:
+        if n==0:
+            mass+=0.5*pdfs[0]*(xs[1]-xs[0])
+        if n==last_index:
+            mass+=0.5*pdfs[-1]*(xs[-2]-xs[-1])
         else:
-            return 1e4
-    HDI_lowTailPr = fmin(intervalWidth, 1.-credint)[0]
-    return ppf(HDI_lowTailPr), ppf(HDI_lowTailPr+credint)
+            mass+=0.5*pdfs[n]*(xs[n+1]-xs[n-1])
+        if mass >= credint:
+            print mass
+            pdf_thresh = pdfs[n]
+            print pdf_thresh
+            break
+    idx1 = argmax(pdfs>=pdf_thresh)
+    idx2 = argmax(pdfs[::-1]>=pdf_thresh)
+    return xs[idx1], xs[::-1][idx2]
 
-def MAP(y, x, res = 1000):
+def MAP(y, x, res = 100):
     """Find the maximum a posteriori value
 
     Parameters:
@@ -47,8 +48,8 @@ def MAP(y, x, res = 1000):
         x (numpy array, same dimensions as y):
             the grid points at which the PDF values y are evaluated
 
-	res (int, optional)
-	    the desired precision of the output in decimal places
+        res (int, optional)
+            the desired precision of the output in decimal places
     Returns:
 
         the highest-probability value
@@ -57,3 +58,20 @@ def MAP(y, x, res = 1000):
     x_highres = linspace(x[0], x[-1], int(around((x[-1]-x[0])*res)))
     y_highres = sp(x_highres)
     return x_highres[argmax(y_highres)]
+
+def credible_levels(ps, fs):
+    """Find the probability levels that correspond to a given array of credible levels, e.g. to then pass into the levels argument of a contour plot
+    Parameters:
+       ps (1-d numpy array):
+          the PDF values, a flattened array
+       fs (1-d numpy array):
+          the desired credibile levels in ascending order, e.g. array([0.5,0.9]) for 50% and 90% credible levels
+    """
+    sorter = argsort(ps)
+    ps_sorted = ps[sorter]
+    ps_sorted = ps_sorted[::-1] #largest to smallest
+    csum = cumsum(ps_sorted)
+    idxs = searchsorted(csum,fs*csum[-1])
+    levels = ps_sorted[idxs]
+    levels = levels[::-1]
+    return levels
