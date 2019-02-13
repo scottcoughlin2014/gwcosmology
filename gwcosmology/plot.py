e2416072e437198d6010ed3b1bea573ed92bb1d9
Maya Fishbach <maya.fishbach@ldas-grid.ligo.caltech.edu>
2019-02-13 08:01:38 -0800

    plotting script

---
+++ gwcosmology/plot.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+from gwcosmology.utils import H0_plot_name
+
+def plot_ho_likelihood(fname):
+     "takes the hdf5 file containing the H0 likelihood for a single event and plots it"
+     with h5py.File(str(fname),'r') as inp:
+          lh = np.array(inp['H0'])
+          hs = inp['H0'].attrs['H0_start']+inp['H0'].attrs['dH0']*np.arange(inp['H0'].attrs['num_H0'])
+          skymap_file = str(inp['H0'].attrs['skymap_file'])
+          gid = str(inp['H0'].attrs['graceid'])
+     plt.plot(hs,lh)
+     plt.xlabel('H0')
+     plt.ylabel('p(H0)')
+     #change this to return figure object
+     plt.savefig(H0_plot_name(gid,skymap_file)) 
--- gwcosmology/utils.py
+++ gwcosmology/utils.py
@@ -12,3 +12,8 @@ def h0_name(graceid,chosen_skymap):
     "generate a standard filename for H0 posterior"
     skyname = chosen_skymap.split('.')[0]
     return "H0-%s-%s.h5"%(graceid,skyname)
+
+def H0_plot_name(gid,chosen_skymap):
+    "generate a standard filename for H0 plot"
+     skyname = chosen_skymap.split('.')[0]
+     return "H0-%s-%s.png"%(gid,skyname)
