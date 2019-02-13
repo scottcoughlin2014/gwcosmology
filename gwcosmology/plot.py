f95a49d97dbaa993acdf9a513ffa49d443d36709
Maya Fishbach <maya.fishbach@ldas-grid.ligo.caltech.edu>
2019-02-13 08:15:07 -0800

    changed plotting to reading

--- gwcosmology/plot.py
+++ gwcosmology/plot.py
@@ -3,15 +3,16 @@ import matplotlib.pyplot as plt
 import h5py
 from gwcosmology.utils import H0_plot_name
 
-def plot_ho_likelihood(fname):
+def read_ho_likelihood(fname):
      "takes the hdf5 file containing the H0 likelihood for a single event and plots it"
      with h5py.File(str(fname),'r') as inp:
           lh = np.array(inp['H0'])
-          hs = inp['H0'].attrs['H0_start']+inp['H0'].attrs['dH0']*np.arange(inp['H0'].attrs['num_H0'])
+          hs = float(inp['H0'].attrs['H0_start'])+float(inp['H0'].attrs['dH0'])*np.arange(inp['H0'].attrs['num_H0'])
           skymap_file = str(inp['H0'].attrs['skymap_file'])
           gid = str(inp['H0'].attrs['graceid'])
-     plt.plot(hs,lh)
-     plt.xlabel('H0')
-     plt.ylabel('p(H0)')
+     return hs, lh
+     #plt.plot(hs,lh)
+     #plt.xlabel('H0')
+     #plt.ylabel('p(H0)')
      #change this to return figure object
-     plt.savefig(H0_plot_name(gid,skymap_file)) 
+     #plt.savefig(H0_plot_name(gid,skymap_file)) 
